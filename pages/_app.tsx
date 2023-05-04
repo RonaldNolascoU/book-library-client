61e333b16dfd5655d65eafded13cafc2787900af
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2023-05-03 22:51:12 -0600

    Add GraphQL client with Apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+// pages/_app.tsx
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
+
+export default MyApp
